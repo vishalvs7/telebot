77d96a36c6de725194c7dd5aec2f5938abf86fff
vishalvs7 <vs7361455@gmail.com>
2023-04-24 16:26:20 +0530

    final commit

--- telebot.py
+++ telebot.py
@@ -21,6 +21,7 @@ username = "Lonewolfvs7"
 # Create the client and connect
 client = TelegramClient(username, api_id, api_hash)
 
+
 async def main(phone):
     await client.start()
     print("Client Created")
@@ -52,28 +53,27 @@ async def main(phone):
             my_channel, ChannelParticipantsSearch(''), offset, limit,
             hash=0
         ))
+       
+       
         if not participants.users:
-            break
-        all_participants.extend(participants.users)
-        offset += len(participants.users)
 
-    all_user_details = []
-    for participant in all_participants:
-        all_user_details.append(
-            {"user.id": participant.id, 
-            "first_name": participant.first_name, 
-            "last_name": participant.last_name,
-            "username": participant.username, 
-            "phone": "a_"+str(participant.phone),
-            "user.access_hash": participant.access_hash,
-            "is_bot": participant.bot})
+            all_user_details = []
+            for participant in all_participants:
+                all_user_details.append(
+                    {"user.id": participant.id,
+                    "first_name": participant.first_name,
+                    "last_name": participant.last_name,
+                    "username": participant.username,
+                    "phone": "a_"+str(participant.phone),
+                    "user.access_hash": participant.access_hash,
+                    "is_bot": participant.bot})
 
-    #with open('user_data.json', 'w') as outfile:
+    # with open('user_data.json', 'w') as outfile:
     #    json.dump(all_user_details, outfile)
 
-    filename = user_input_channel.replace("/","_").replace(":","")
+    filename = user_input_channel.replace("/", "_").replace(":", "")
     df = pd.DataFrame(all_user_details)
-    df.to_csv('withphone_'+filename+'.csv', index = True) # column names ekler
+    df.to_csv('withphone_'+filename+'.csv', index=True)  # column names ekler
 
 with client:
     client.loop.run_until_complete(main(phone))
